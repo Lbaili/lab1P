9828514a9bccc0f21085b4fb602076f279a2cc06
Chang Li <1295118952@qq.com>
2020-09-01 14:15:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+Tem= input("Enter temperature: ")
+Tem=float(Tem)
+Typ=input("Enter unit in F/f or C/c: ")
+if Typ=='C' or Typ=='c': print(str(Tem)+"° in Celsius is equivalent to "+str(Tem*1.8+32.0) +"° Fahrenheit.")
+elif Typ =='F' or Typ=='f': print(str(Tem)+"° in  Fahrenheit is equivalent to "+str((Tem-32.0)/1.8) +"° Celsius.")
+else: print("Invalid unit("+Typ+").")
